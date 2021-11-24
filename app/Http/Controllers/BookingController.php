a7e55911cf475afefb812fe5139e4719838b1533
Adrian Lactuan <aplactuan@gmail.com>
2021-11-24 23:26:29 +0800

    Employee slot availability

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,32 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Bookings\Filters\AppointmentFilter;
-use App\Bookings\Filters\SlotsPassedTodayFilter;
-use App\Bookings\Filters\UnavailabilityFilter;
-use App\Bookings\TimeSlotGenerator;
-use App\Models\Appointment;
+use App\Models\Employee;
 use App\Models\Schedule;
 use App\Models\Service;
-use Carbon\CarbonInterval;
-use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
     public function __invoke()
     {
         $schedule = Schedule::find(1);
-        $service = Service::find(2);
+        $service = Service::find(1);
 
-        $appointments = Appointment::whereDate('date', '2021-11-03')->get();
+        $employee = Employee::find(1);
 
-        $slots = (new TimeSlotGenerator($schedule, $service))
-            ->applyFilters([
-                new SlotsPassedTodayFilter(),
-                new UnavailabilityFilter($schedule->unavailabilities),
-                new AppointmentFilter($appointments)
-            ])
-            ->get();
+        $slots = $employee->availableTimeSlots($schedule, $service);
 
         return view('bookings.create', [
             'slots' => $slots
