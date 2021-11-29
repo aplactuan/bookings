98d457c164584f72cd84623b4b92377dbce5f8b4
Adrian Lactuan <aplactuan@gmail.com>
2021-11-29 21:27:13 +0800

    Add list of employees when user select a service

--- app/Http/Livewire/CreateBooking.php
+++ app/Http/Livewire/CreateBooking.php
@@ -7,9 +7,33 @@ use Livewire\Component;
 
 class CreateBooking extends Component
 {
+    public $employees;
+
     public $state = [
-        'service' => null
+        'service' => '',
+        'employee' => ''
     ];
+
+    public function updatedStateService($serviceId)
+    {
+        $this->state['employee'] = null;
+
+        $this->employees = $this->selectedService->employees;
+    }
+
+    public function mount()
+    {
+        $this->employees = collect();
+    }
+
+    public function getSelectedServiceProperty()
+    {
+        if (!$this->state['service']) {
+            return '';
+        }
+
+        return Service::find($this->state['service']);
+    }
 
     public function render()
     {
--- resources/views/livewire/create-booking.blade.php
+++ resources/views/livewire/create-booking.blade.php
@@ -1,10 +1,22 @@
 <div class="bg-gray-200 max-w-sm mx-auto m-6 p-5 rounded-lg">
     <form>
-        <label for="service" class="inline-block text-gray-700 font-bold mb-2">Select Service</label>
-        <select name="service" id="service" class="bg-white h-10 w-full border-none rounded-lg" wire:model="state.service">
-            @foreach($services as $service)
-                <option value="{{ $service->id }}">{{ $service->name }}</option>
-            @endforeach
-        </select>
+        <div class="mb-6">
+            <label for="service" class="inline-block text-gray-700 font-bold mb-2">Select Service</label>
+            <select name="service" id="service" class="bg-white h-10 w-full border-none rounded-lg" wire:model="state.service">
+                <option value="">Select a service</option>
+                @foreach($services as $service)
+                    <option value="{{ $service->id }}">{{ $service->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="mb-6">
+            <label for="employee" class="inline-block text-gray-700 font-bold mb-2">Select Employee</label>
+            <select name="employee" id="employee" class="bg-white h-10 w-full border-none rounded-lg" wire:model="state.employee">
+                <option value="">Select an employee</option>
+                @foreach($employees as $employee)
+                    <option value="{{ $employee->id }}">{{ $employee->name }}</option>
+                @endforeach
+            </select>
+        </div>
     </form>
 </div>
